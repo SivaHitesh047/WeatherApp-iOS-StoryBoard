caaf2e9505d6027027ce80612089a7f3e25880da
SivaHitesh047 <sivahitesh@Siva-Hiteshs-MacBook-Pro.local>
2021-08-16 22:14:32 +0530

    Initial commit

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Siva Hitesh Kasturi on 29/05/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Siva Hitesh Kasturi on 14/06/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
